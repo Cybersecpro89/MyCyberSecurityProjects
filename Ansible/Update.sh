28a9c70d6bbbb313f7fef2a0ebfbc97bf380bb14
Cybersecpro89 <74457291+Cybersecpro89@users.noreply.github.com>
2020-11-21 07:43:13 -0500

    Delete Update.sh

--- Ansible/Update.sh
+++
@@ -1,7 +0,0 @@
-update.sh
-#!/bin/bash
-apt update -y
-apt upgrade -y
-apt full-upgrade -y
-apt autoremove --purge -y
-apt update -y && apt upgrade -y && apt full-upgrade -y && apt-get autoremove --purge -y
